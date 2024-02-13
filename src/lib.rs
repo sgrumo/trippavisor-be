b99cb5ddf4e80a0551820786edd0afd95272b9e3
Samuele Medici <samuele.medici@quinck.io>
2024-02-13 17:24:08 +0100

    feat: :tada: init project

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+pub mod utils;
+
+use worker::*;
+
+use crate::utils::models::festival::*;
+
+const DATO_CMS_URL: &str = "https://graphql.datocms.com/";
+
+#[event(fetch)]
+async fn main(mut req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let client = reqwest::Client::new();
+    let body = req.bytes().await?;
+
+    let body = reqwest::Body::from(body);
+
+    let bearer = match env.secret("BEARER_TOKEN") {
+        Ok(value) => value.to_string(),
+        Err(err) => return Response::error(format!("Internal Server Error: {err}"), 500),
+    };
+
+    let res = client
+        .post(DATO_CMS_URL)
+        .body(body)
+        .header("Authorization", format!("Bearer {bearer}"))
+        .send()
+        .await;
+
+    let response_body = match res {
+        Ok(response) => match response.bytes().await {
+            Ok(bytes) => bytes.to_vec(),
+            Err(err) => return Response::error(format!("Internal Server Error: {err}"), 500),
+        },
+        Err(err) => return Response::error(format!("Internal Server Error: {err}"), 500),
+    };
+
+    let serialized: FestivalsResponse = match serde_json::from_slice(&response_body) {
+        Ok(value) => value,
+        Err(err) => return Response::error(format!("Internal Server Error: {err}"), 500),
+    };
+
+    let origins = ["*"];
+
+    let cors_headers: Vec<&str> = vec!["Content-Type"];
+
+    let cors = Cors::default()
+        .with_allowed_headers(cors_headers)
+        .with_origins(origins)
+        .with_methods(Method::all());
+
+    match Response::from_json(&serialized) {
+        Ok(response) => response.with_cors(&cors),
+        Err(err) => return Response::error(format!("Internal Server Error: {err}"), 500),
+    }
+}
---
+++ src/utils/models/festival.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FestivalsResponse {
+    pub data: FestivalData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FestivalData {
+    pub allFestivals: Vec<Festival>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Festival {
+    pub title: String,
+    pub id: String,
+    pub slug: String,
+    pub address: Address,
+    pub period: Vec<Period>,
+    pub tags: Vec<Tag>,
+    pub thumbnail: Thumbnail,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Address {
+    pub route: String,
+    pub locality: String,
+    pub administrative_area_level_3: String,
+    pub administrative_area_level_2: String,
+    pub administrative_area_level_1: String,
+    pub country: String,
+    pub postal_code: String,
+    pub name: String,
+    pub formatted_address: String,
+    pub coordinates: Coordinates,
+    pub utc_offset_minutes: i32,
+    pub street_number: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Coordinates {
+    pub lat: f64,
+    pub lng: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Period {
+    pub startdate: String,
+    pub enddate: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tag {
+    pub tag: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Thumbnail {
+    pub responsiveImage: ResponsiveImage,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResponsiveImage {
+    pub src: String,
+    pub srcSet: String,
+    pub width: u32,
+    pub height: u32,
+    pub alt: String,
+    pub sizes: String,
+}
